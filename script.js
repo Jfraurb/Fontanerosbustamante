c064adfdc8692219a689e0355d812212871df53c
Jorge Fradeja <106486017+Jfraurb@users.noreply.github.com>
2026-03-16 10:20:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+if (menuToggle && mobileMenu) {
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+  });
+
+  mobileMenu.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => mobileMenu.classList.remove('open'));
+  });
+}
